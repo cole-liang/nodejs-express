01d1bd76b1b75670b7714dbb536a132db17588e2
cole <xdxlwx@gmail.com>
2019-07-26 15:19:23 +1000

    setup mongoose, get items with query

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-
+const bookRouter = express.Router();
+const db = mongoose.connect('mongodb://localhost/bookAPI');
 const port = process.env.PORT || 3000;
+const Books = require('./models/bookModel');
+
+bookRouter.route('/books').get((req, res) => {
+  const query = {};
+  if (req.query.genre) {
+    query.genre = req.query.genre;
+  }
+
+  Books.find(query, (err, books) => (err ? res.send(err) : res.json(books)));
+});
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to Node');
